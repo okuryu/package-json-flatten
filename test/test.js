67e7b4132de1d22b4e0c60d389b9995d1cb633c4
Ryuichi Okumura <okuryu@okuryu.com>
2014-12-29 18:03:04 +0900

    Use multiple var syntax

--- test/test.js
+++ test/test.js
@@ -6,82 +6,82 @@
 /*global describe, it*/
 "use strict";
 
-var flatten = require("../dist/flatten"),
-    expect = require("chai").expect;
+var flatten = require("../dist/flatten");
+var expect = require("chai").expect;
 
 describe("Basic Test", function () {
     it("flatten normal data", function () {
         var data = {
-                description: "Test Data",
-                version: "0.0.1",
-                name: "Test Data"
-            },
-            expectedData = {
-                name: "Test Data",
-                version: "0.0.1",
-                description: "Test Data"
-            },
-            flattenedData = flatten(data);
+            description: "Test Data",
+            version: "0.0.1",
+            name: "Test Data"
+        };
+        var expectedData = {
+            name: "Test Data",
+            version: "0.0.1",
+            description: "Test Data"
+        };
+        var flattenedData = flatten(data);
         expect(JSON.stringify(flattenedData)).to.be.equal(JSON.stringify(expectedData));
     });
     it("flatten extended data", function () {
         var data = {
-                yahoo: {},
-                description: "Test Data",
-                version: "0.0.1",
-                name: "Test Data"
-            },
-            expectedData = {
-                name: "Test Data",
-                version: "0.0.1",
-                description: "Test Data",
-                yahoo: {}
-            },
-            flattenedData = flatten(data);
+            yahoo: {},
+            description: "Test Data",
+            version: "0.0.1",
+            name: "Test Data"
+        };
+        var expectedData = {
+            name: "Test Data",
+            version: "0.0.1",
+            description: "Test Data",
+            yahoo: {}
+        };
+        var flattenedData = flatten(data);
         expect(JSON.stringify(flattenedData)).to.be.equal(JSON.stringify(expectedData));
     });
     it("flatten data contains keywords", function () {
         var data = {
-                keywords: [
-                    "foo",
-                    "bar"
-                ],
-                description: "Test Data",
-                version: "0.0.1",
-                name: "Test Data"
-            },
-            expectedData = {
-                name: "Test Data",
-                version: "0.0.1",
-                description: "Test Data",
-                keywords: [
-                    "bar",
-                    "foo"
-                ]
-            },
-            flattenedData = flatten(data);
+            keywords: [
+                "foo",
+                "bar"
+            ],
+            description: "Test Data",
+            version: "0.0.1",
+            name: "Test Data"
+        };
+        var expectedData = {
+            name: "Test Data",
+            version: "0.0.1",
+            description: "Test Data",
+            keywords: [
+                "bar",
+                "foo"
+            ]
+        };
+        var flattenedData = flatten(data);
         expect(JSON.stringify(flattenedData)).to.be.equal(JSON.stringify(expectedData));
     });
     it("flatten data contains files", function () {
         var data = {
-                files: [
-                    "foo",
-                    "bar"
-                ],
-                description: "Test Data",
-                version: "0.0.1",
-                name: "Test Data"
-            },
-            expectedData = {
-                name: "Test Data",
-                version: "0.0.1",
-                description: "Test Data",
-                files: [
-                    "bar",
-                    "foo"
-                ]
-            },
-            flattenedData = flatten(data);
+            files: [
+                "foo",
+                "bar"
+            ],
+            description: "Test Data",
+            version: "0.0.1",
+            name: "Test Data"
+        };
+        var expectedData = {
+            name: "Test Data",
+            version: "0.0.1",
+            description: "Test Data",
+            files: [
+                "bar",
+                "foo"
+            ]
+        };
+        var flattenedData = flatten(data);
         expect(JSON.stringify(flattenedData)).to.be.equal(JSON.stringify(expectedData));
     });
 });
