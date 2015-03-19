9b2904d05761541b6fa0de800e53782cfe82cff6
Ryuichi Okumura <okuryu@okuryu.com>
2015-03-19 20:50:38 +0900

    Use eslint instead of jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,8 @@ module.exports = function (grunt) {
         clean: {
             main: ["build", "dist"]
         },
-        jshint: {
-            options: {
-                jshintrc: true
-            },
-            main: ["Gruntfile.js", "lib", "bin", "test"]
+        eslint: {
+            main: ["*.js", "lib", "bin", "test"]
         },
         transpile: {
             main: {
@@ -28,7 +25,7 @@ module.exports = function (grunt) {
                 }]
             }
         },
-        es6_module_wrap_default: {
+        es6_module_wrap_default: { // eslint-disable-line camelcase
             options: {
                 type: "cjs"
             },
@@ -39,7 +36,7 @@ module.exports = function (grunt) {
                 dest: "dist"
             }
         },
-        mocha_istanbul: {
+        mocha_istanbul: { // eslint-disable-line camelcase
             options: {
                 coverage: true
             },
@@ -55,13 +52,13 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-es6-module-transpiler");
     grunt.loadNpmTasks("grunt-es6-module-wrap-default");
+    grunt.loadNpmTasks("grunt-eslint");
     grunt.loadNpmTasks("grunt-mocha-istanbul");
     grunt.loadNpmTasks("grunt-coveralls");
 
-    grunt.registerTask("lint", ["jshint"]);
+    grunt.registerTask("lint", ["eslint"]);
     grunt.registerTask("build", ["transpile", "es6_module_wrap_default"]);
     grunt.registerTask("test", ["mocha_istanbul"]);
     grunt.registerTask("default", ["clean", "lint", "build", "test"]);
